5f2d0f614d672fd2d3708e279f60cc27617a7e44
Lee <leeklopfers@gmx.de>
2021-12-10 20:17:59 +0100

    init

---
+++ WiredBrainAppStackAppTest/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace WiredBrainAppStackApp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestPushMethod()
+        {
+            var stack = new SimpleStack();
+            double sum = 0.0;
+
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Assert.Equal(7.0, sum, 1);
+        }
+    }
+}

55dea75310ee2f14150371d6977d83552e42d089
Lee <leeklopfers@gmx.de>
2021-12-11 23:54:33 +0100

    refactor

---
+++ WiredBrainAppStackAppTest/UnitTest1 (another copy).cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace WiredBrainAppStackApp
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestPushMethod()
+        {
+            var stackDouble = new SimpleStack<double>();
+            double sum = 0.0;
+
+            stackDouble.Push(1.2);
+            stackDouble.Push(2.8);
+            stackDouble.Push(3.0);
+
+
+            while (stackDouble.Count > 0)
+            {
+                double item = stackDouble.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+
+            Assert.Equal(7.0, sum, 1);
+        }
+    }
+}
